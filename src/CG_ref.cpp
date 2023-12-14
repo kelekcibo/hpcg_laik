43102e888b21b99cad1c971f796a8370fa446f08
kelekci <ibrahim.erdurucan@tum.de>
2023-12-14 10:58:39 +0100

    Adjusted back to calculate CG SETS

--- src/CG_ref.cpp
+++ src/CG_ref.cpp
@@ -31,7 +31,7 @@
 #include "ComputeWAXPBY_ref.hpp"
 
 // Use TICK and TOCK to time a code section in MATLAB-like fashion
-#define TICK()  t0 = mytimer() //!< record current time in 't0'
+#define TICK() t0 = mytimer()       //!< record current time in 't0'
 #define TOCK(t) t += mytimer() - t0 //!< store time difference in 't' using time in 't0'
 
 #ifndef HPCG_NO_LAIK
@@ -55,8 +55,8 @@
   @see CG()
 */
 int CG_laik_ref(SparseMatrix &A, CGData &data, Laik_Blob *b, Laik_Blob *x,
-           const int max_iter, const double tolerance, int &niters, double &normr, double &normr0,
-           double *times, bool doPreconditioning)
+                const int max_iter, const double tolerance, int &niters, double &normr, double &normr0,
+                double *times, bool doPreconditioning)
 {
 
   double t_begin = mytimer(); // Start timing right away
@@ -67,16 +67,13 @@ int CG_laik_ref(SparseMatrix &A, CGData &data, Laik_Blob *b, Laik_Blob *x,
   // #ifndef HPCG_NO_MPI
   //   double t6 = 0.0;
   // #endif
-  double t_rep = 0;
-  double t_it_before = 0;
-  double t_it_after = 0;
 
   local_int_t nrow = A.localNumberOfRows;
 
-  Laik_Blob * r = data.r_blob; // Residual vector
-  Laik_Blob * z = data.z_blob;  // Preconditioned residual vector
-  Laik_Blob * p = data.p_blob;   // Direction vector (in MPI mode ncol>=nrow)
-  Laik_Blob * Ap = data.Ap_blob;
+  Laik_Blob *r = data.r_blob; // Residual vector
+  Laik_Blob *z = data.z_blob; // Preconditioned residual vector
+  Laik_Blob *p = data.p_blob; // Direction vector (in MPI mode ncol>=nrow)
+  Laik_Blob *Ap = data.Ap_blob;
 
   if (!doPreconditioning && A.geom->rank == 0)
     HPCG_fout << "WARNING: PERFORMING UNPRECONDITIONED ITERATIONS" << std::endl;
@@ -95,7 +92,7 @@ int CG_laik_ref(SparseMatrix &A, CGData &data, Laik_Blob *b, Laik_Blob *x,
 #endif
 
 #ifdef REPARTITION
-  if(A.repartition_me)
+  if (A.repartition_me)
   {
     if (iter == 0)
     {
@@ -170,102 +167,77 @@ int CG_laik_ref(SparseMatrix &A, CGData &data, Laik_Blob *b, Laik_Blob *x,
   int k = 1;
 
 #ifdef REPARTITION
-  if(A.repartition_me && laik_phase(hpcg_instance) > 0)
+  if (A.repartition_me && laik_phase(hpcg_instance) > 0)
   {
     k = laik_phase(hpcg_instance); // should equal 11
     assert(k == 11);
   }
 #endif
 
-  double t_before_start = 0;
-  double t_after_start = 0;
-  int k_before = 0;
-  int k_after = 0;
-  double local_time = 0;
-  int max_iter2 = 100;
-  for (; k <= max_iter2 && normr / normr0 > tolerance; k++)
+  for (; k <= max_iter && normr / normr0 > tolerance; k++)
   {
-    if (k <= 50)
-    {
-      t_before_start = mytimer();
-      k_before++;
-    }
-    else if (k > 50 && k <= 100)
-    {
-      t_after_start = mytimer();
-      k_after++;
-    }
+    // if (k == 11 && laik_size(world) == 2)
+    // {
+    //   std::string debug{"\x1B[33m"};
+    //   debug += "LAIK " + to_string(laik_myid(world)) + "\t";
+    //   debug += "Checkpoint END\x1B[0m";
+    //   printf("%s\n", debug.data());
+    //   exit_hpcg_run("free()/malloc() error", false);
+    // }
 
-    // TICK();
+    TICK();
     if (doPreconditioning)
       ComputeMG_laik_ref(A, r, z); // Apply preconditioner
     else
       ComputeWAXPBY_laik_ref(nrow, 1.0, r, 0.0, r, z); // copy r to z (no preconditioning)
-    // TOCK(t5); // Preconditioner apply time
+    TOCK(t5);                                          // Preconditioner apply time
 
     if (k == 1)
     {
       CopyLaikVectorToLaikVector(z, p);
-      // // TOCK(t2); // Copy Mr to p
-      // TICK();
+      TOCK(t2); // Copy Mr to p
+      TICK();
       ComputeDotProduct_laik_ref(nrow, r, z, rtz, t4);
-      // TOCK(t1); // rtz = r'*z
+      TOCK(t1); // rtz = r'*z
     }
     else
     {
       oldrtz = rtz;
-      // TICK();
+      TICK();
       ComputeDotProduct_laik_ref(nrow, r, z, rtz, t4);
-      // TOCK(t1); // rtz = r'*z
+      TOCK(t1); // rtz = r'*z
       beta = rtz / oldrtz;
-      // TICK();
+      TICK();
       ComputeWAXPBY_laik_ref(nrow, 1.0, z, beta, p, p);
-      // TOCK(t2); // p = beta*p + z
+      TOCK(t2); // p = beta*p + z
     }
 
-    // TICK();
+    TICK();
     ComputeSPMV_laik_ref(A, p, Ap);
-    // TOCK(t3); // Ap = A*p
-    // TICK();
+    TOCK(t3); // Ap = A*p
+    TICK();
     ComputeDotProduct_laik_ref(nrow, p, Ap, pAp, t4);
-    // TOCK(t1); // alpha = p'*Ap
+    TOCK(t1); // alpha = p'*Ap
     alpha = rtz / pAp;
-    // TICK();
+    TICK();
     ComputeWAXPBY_laik_ref(nrow, 1.0, x, alpha, p, x); // x = x + alpha*p
     ComputeWAXPBY_laik_ref(nrow, 1.0, r, -alpha, Ap, r);
-    // TOCK(t2); // r = r - alpha*Ap
-    // TICK();
+    TOCK(t2); // r = r - alpha*Ap
+    TICK();
     ComputeDotProduct_laik_ref(nrow, r, r, normr, t4);
-    // TOCK(t1);
+    TOCK(t1);
     normr = sqrt(normr);
 #ifdef HPCG_DEBUG
     if (A.geom->rank == 0 && (k % print_freq == 0 || k == max_iter))
       HPCG_fout << "Iteration = " << k << "   Scaled Residual = " << normr / normr0 << std::endl;
 #endif
     niters = k;
-    if (k <= 10)
-    {
-      double local_time = mytimer() - t_before_start;
-      // printf("Before repart_Iteration %d \t local_time: %.25f seconds\n", k, local_time);
-
-      if (k <= 50)
-      {
-        local_time = mytimer() - t_before_start;
-        t_it_before += local_time; // worst time of all iterations before repartitioning
-      }
-      else if (k > 50 && k <= 100)
-      {
-        local_time = mytimer() - t_after_start;
-        t_it_after += local_time; // worst time of all iterations before repartitioning
-      }
-    }
 
 #ifdef REPARTITION
     // Repartitioning / Resizing of current world (group of proccesses) in the 10th iteration
     // For now, repartitioning is only done once
-    if (k == 50 && A.repartition_me && !A.repartitioned)
+    if (k == 10 && A.repartition_me && !A.repartitioned)
     {
-      TICK();
       A.repartition_me = false;             // do not repartition the matrix anymore
       uint32_t old_size = laik_size(world); // Old world size for output
 
@@ -295,7 +267,6 @@ int CG_laik_ref(SparseMatrix &A, CGData &data, Laik_Blob *b, Laik_Blob *x,
         repartition_SparseMatrix(A);
         nrow = A.localNumberOfRows; /* update local value after repartitioning */
 
-   
         // Switch to the new partitioning on all Laik_data containers
         std::vector<Laik_Blob *> list{};
         /* Vectors in MG_data will be recursively handled in re_switch_LaikVectors */
@@ -311,10 +282,10 @@ int CG_laik_ref(SparseMatrix &A, CGData &data, Laik_Blob *b, Laik_Blob *x,
         // laik_free_partitioning(old_local); // TODO fix double free (occurs when removed processes free old partitionings)
         // laik_free_partitioning(old_ext);
         // Need to send normr to new joining procs
-        if(new_size > old_size)
+        if (new_size > old_size)
         {
-          laik_broadcast((void *)&normr0, (void *) &normr0, 1, laik_Double); // TODO Use partial broadcast (old processes do not need to parcipate in the computation)
-          laik_broadcast((void *)&normr, (void *) &normr, 1, laik_Double);
+          laik_broadcast((void *)&normr0, (void *)&normr0, 1, laik_Double); // TODO Use partial broadcast (old processes do not need to parcipate in the computation)
+          laik_broadcast((void *)&normr, (void *)&normr, 1, laik_Double);
           laik_broadcast((void *)&rtz, (void *)&rtz, 1, laik_Double);
         }
 
@@ -334,25 +305,11 @@ int CG_laik_ref(SparseMatrix &A, CGData &data, Laik_Blob *b, Laik_Blob *x,
 
         A.repartitioned = true;
         HPCG_fout << "REPARTIONING: Old world size [" << old_size << "] New world size [" << new_size << "]" << std::endl;
-        TOCK(t_rep);
+        printf("################### LAIK %d \t REPARTIONING DONE -> NEW WORLD SIZE %d\n\n", laik_myid(world), new_size); // DELETE ME
       }
     }
 #endif // REPARTITION
   }
-
-  t_it_before = (t_it_before / (double)k_before);
-  t_it_after = (t_it_after / (double)k_after);
-
-  // if(A.geom->rank == 0)
-  {
-    std::string result{"LAIK \t"};
-    result += to_string(laik_myid(world)) + "\n";
-    result += "avg before repartitioning: " + to_string(t_it_before) + " seconds\n";
-    result += "avg after repartitioning: " + to_string(t_it_after) + " seconds\n";
-    result += "repartitioning time: " + to_string(t_rep) + " seconds\n";
-    std::cout << result;
-  }
-
 
   // Store times
   times[1] += t1; // dot product time
